30a822b523320e5d90edf919fb5dc2a49c00584a
Kalle <kalle@kandru.de>
2025-04-03 22:09:11 +0200

    bump version to 0.0.2

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace ChatInfo
 {
     public partial class ChatInfo
     {
-        public override string ModuleVersion => "0.0.1";
+        public override string ModuleVersion => "0.0.2";
     }
 }
